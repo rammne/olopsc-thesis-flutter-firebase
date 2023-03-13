5902ad4f670a1dc4bf90344d908c6eb436dd37ef
rammne <rametiongson@gmail.com>
2023-03-14 01:29:03 +0800

    optimized for admin search function

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -21,11 +21,23 @@ class AuthService {
       UserCredential result = await FirebaseAuth.instance
           .createUserWithEmailAndPassword(email: email, password: password);
       User? user = result.user;
+
+      setSearchParam(String studentNumber) {
+        List<String> caseSearchList = [];
+        String temp = "";
+        for (int i = 0; i < studentNumber.length; i++) {
+          temp = temp + studentNumber[i];
+          caseSearchList.add(temp);
+        }
+        return caseSearchList;
+      }
+
       await FirebaseFirestore.instance.collection('users').doc(user!.uid).set({
         'full_name': full_name,
         'email': email,
         'program': programLevel,
         'student_number': studentNumber,
+        'student_number_search': setSearchParam(studentNumber),
       });
     } catch (e) {
       print(e.toString());
